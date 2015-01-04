8ca3e48fddff89c851eeb3543398adf4a3044f6b
d-donchev <ddonchev@gmail.com>
2015-01-04 23:19:24 +0200

    Added Town Service

---
+++ app/js/services/TownService.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+app.factory('townService', ['$resource', 'mainUrl',  function ($resource, mainUrl) {
+    var adsResource = $resource(mainUrl + '/towns', null, {
+        'getAll': { method: 'GET', isArray: false }
+    });
+
+    return {
+        getAll: function () {
+             return adsResource.getAll().$promise;
+        }
+    }
+}]);
