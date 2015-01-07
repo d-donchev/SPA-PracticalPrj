daa576e438b4a7341dca3713457b4af697b3ca60
d-donchev <ddonchev@gmail.com>
2015-01-07 22:01:30 +0200

    Structural changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,22 @@
 'use strict';
-var app = angular.module('adv', ['ngResource'])
+var app = angular.module('adv', ['ngResource', 'ngRoute'])
     .constant('mainUrl', 'http://softuni-ads.azurewebsites.net/api')
     .constant('pageSize', 10);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/templates/home.html',
+        controller: 'HomeController'
+    });
+    $routeProvider.when('/', {
+        templateUrl: '/templates/login.html',
+        controller: 'LoginController'
+    });
+    $routeProvider.when('/', {
+        templateUrl: '/templates/register.html',
+        controller: 'RegisterController'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+}]);
