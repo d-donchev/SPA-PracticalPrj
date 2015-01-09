fcf5aede18f7c09332e4479174fbc6bf1b0c16b2
d-donchev <ddonchev@gmail.com>
2015-01-09 23:24:05 +0200

    Small Fix

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -1,5 +1,14 @@
 ﻿'use strict';
 
-app.controller('RegisterController', ['$scope', function ($scope) {
+app.controller('RegisterController', ['$scope', 'townsData', function ($scope) {
+    townsData.getTowns()
+    .$promise
+    .then(function (data) {
+        $scope.towns = data;
+    });
 
+    $scope.register = function (user) {
+        console.log(user);
+        //userData.register(user);
+    }
 }]);
--- app/js/services/data/userData.js
+++ app/js/services/data/userData.js
@@ -1,1 +1,18 @@
-﻿
+﻿app.factory('userData', ['$resource', function ($resource) {
+    function registerUser(user) {
+
+    }
+    function loginUser(user) {
+
+    }
+
+    function logoutUser(user) {
+
+    }
+
+    return {
+        register: registerUser,
+        login: loginUser,
+        logout: logoutUser
+    }
+}]);
